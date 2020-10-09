c1e81d4d8a6e3e3d64c1a3f88b61895c55504508
GabrielFarias9989 <ayrtongabriel9989@gmail.com>
2020-10-09 16:25:09 -0300

    First commit

---
+++ src/main/java/com/hotel_aeso/HOTEL_AESO/HotelAesoApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_aeso.HOTEL_AESO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel_aeso/HOTEL_AESO/HotelAesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_aeso.HOTEL_AESO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
